be6ee530af4fcfe0a27a167ee823a050c045a21e
Radovan Bast <bast@users.noreply.github.com>
2016-11-21 11:56:42 +0100

    reduce file size

--- example/example.sh
+++ example/example.sh
@@ -4,6 +4,6 @@
 python ../streamline.py --data=plot.2d.example --molecule=benzene.xy --legend
 
 # convert ps to jpg
-convert -trim -density 300 plot.2d.example.ps -quality 80 plot.2d.example.jpg
+convert -trim -density 200 plot.2d.example.ps -quality 80 plot.2d.example.jpg
 
 exit 0
